f57054a80299f6b2c9382f0a86b6a5f7628ae598
lisflower <lisflower85@gmail.com>
2021-10-28 13:48:18 -0400

    version 1

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('/pliegos', function () {
+    return view('pliegos');
+});
+
+Route::get('/vista', function () {
+    return view('vista');
+});
+
+Route::get('/convocatoria', function () {
+    return view('convocatoria');
+});
+
+Route::get('/registro', function () {
+    return view('registro');
+});
